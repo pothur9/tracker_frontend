d88dd8421f4a83c6f1822e62ec1e5492b073116e
pothur909 <prasannakumar@quaantisc.com>
2025-03-01 15:25:29 +0530

    Delete OneDrive/Desktop/easy2trip-raunak-multiCity/easy2trip directory

--- OneDrive/Desktop/easy2trip-raunak-multiCity/easy2trip/backend/src/routes/coupon.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getCoupons } = require("../controllers/coupon.controller");
-
-const router = express.Router();
-
-router.get("/", getCoupons);
-
-module.exports = router;
